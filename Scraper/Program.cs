09ad89f5c29aba6eb3807eff57b6de95fc33cdb5
Martin Doms <iamaelephant@gmail.com>
2020-12-24 06:38:36 +1300

    Initial unfinished commit

---
+++ Scraper/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Scraper
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            await new Mitre10Scraper().Scrape();
+
+        }
+    }
+}
